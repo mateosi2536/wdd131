e0247f0234ddbd192d391d97be0a4f03f952484d
mateosi2536 <mateoszambranos@gmail.com>
2024-09-21 07:50:56 -0500

    I have end the main page

---
+++ scripts/getdates.js
@@ -0,0 +1,11 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+
+// Find the footer's first paragraph
+const copyrightParagraph = document.getElementById("currentyear");
+const lastModifiedParagraph = document.getElementById("lastModified");
+
+// Set the copyright year text
+    copyrightParagraph.textContent = `${new Date().getFullYear()}`;
+    lastModifiedParagraph.textContent = `Last Modification: ${lastModified}`;
